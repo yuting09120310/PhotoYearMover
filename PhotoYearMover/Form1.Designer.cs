63108b56a757b985c218bd39fc8a6dbff3b31d83
yuting09120310 <yuting09120310@gmail.com>
2025-04-23 01:18:47 +0800

    加入專案檔案。

---
+++ PhotoYearMover/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace PhotoYearMover
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2AnimateWindow1 = new Guna.UI2.WinForms.Guna2AnimateWindow(this.components);
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2TextBox1 = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2HtmlLabel3 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.listLogs = new System.Windows.Forms.ListView();
+            this.FileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.CreateDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn_Copy = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Transparent;
+            this.comboBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.comboBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.comboBox1.ItemHeight = 30;
+            this.comboBox1.Location = new System.Drawing.Point(189, 43);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(191, 36);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(63, 47);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(111, 26);
+            this.guna2HtmlLabel1.TabIndex = 1;
+            this.guna2HtmlLabel1.Text = "搜索年份 : ";
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(63, 108);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(111, 26);
+            this.guna2HtmlLabel2.TabIndex = 3;
+            this.guna2HtmlLabel2.Text = "存放位置 : ";
+            // 
+            // guna2TextBox1
+            // 
+            this.guna2TextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.guna2TextBox1.DefaultText = "";
+            this.guna2TextBox1.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.guna2TextBox1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.guna2TextBox1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.guna2TextBox1.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2TextBox1.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.guna2TextBox1.Location = new System.Drawing.Point(189, 108);
+            this.guna2TextBox1.Name = "guna2TextBox1";
+            this.guna2TextBox1.PlaceholderText = "";
+            this.guna2TextBox1.SelectedText = "";
+            this.guna2TextBox1.Size = new System.Drawing.Size(601, 36);
+            this.guna2TextBox1.TabIndex = 4;
+            this.guna2TextBox1.Click += new System.EventHandler(this.guna2TextBox1_Click);
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(301, 508);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(124, 45);
+            this.guna2Button1.TabIndex = 6;
+            this.guna2Button1.Text = "掃瞄作業";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2HtmlLabel3
+            // 
+            this.guna2HtmlLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel3.Font = new System.Drawing.Font("新細明體", 18F);
+            this.guna2HtmlLabel3.Location = new System.Drawing.Point(63, 187);
+            this.guna2HtmlLabel3.Name = "guna2HtmlLabel3";
+            this.guna2HtmlLabel3.Size = new System.Drawing.Size(111, 26);
+            this.guna2HtmlLabel3.TabIndex = 7;
+            this.guna2HtmlLabel3.Text = "作業日誌 : ";
+            // 
+            // listLogs
+            // 
+            this.listLogs.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FileName,
+            this.CreateDate});
+            this.listLogs.HideSelection = false;
+            this.listLogs.Location = new System.Drawing.Point(63, 229);
+            this.listLogs.Name = "listLogs";
+            this.listLogs.Size = new System.Drawing.Size(727, 254);
+            this.listLogs.TabIndex = 8;
+            this.listLogs.UseCompatibleStateImageBehavior = false;
+            this.listLogs.View = System.Windows.Forms.View.Details;
+            // 
+            // FileName
+            // 
+            this.FileName.Text = "檔案名稱";
+            this.FileName.Width = 550;
+            // 
+            // CreateDate
+            // 
+            this.CreateDate.Text = "拍攝日期";
+            this.CreateDate.Width = 150;
+            // 
+            // btn_Copy
+            // 
+            this.btn_Copy.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Copy.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btn_Copy.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btn_Copy.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btn_Copy.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btn_Copy.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Copy.ForeColor = System.Drawing.Color.White;
+            this.btn_Copy.Location = new System.Drawing.Point(461, 508);
+            this.btn_Copy.Name = "btn_Copy";
+            this.btn_Copy.Size = new System.Drawing.Size(124, 45);
+            this.btn_Copy.TabIndex = 9;
+            this.btn_Copy.Text = "複製作業";
+            this.btn_Copy.Click += new System.EventHandler(this.btn_Copy_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 593);
+            this.Controls.Add(this.btn_Copy);
+            this.Controls.Add(this.listLogs);
+            this.Controls.Add(this.guna2HtmlLabel3);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2TextBox1);
+            this.Controls.Add(this.guna2HtmlLabel2);
+            this.Controls.Add(this.guna2HtmlLabel1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "全白方塊 - 數位相片管理器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Guna.UI2.WinForms.Guna2ComboBox comboBox1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2AnimateWindow guna2AnimateWindow1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2TextBox guna2TextBox1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel3;
+        private System.Windows.Forms.ListView listLogs;
+        private System.Windows.Forms.ColumnHeader FileName;
+        private System.Windows.Forms.ColumnHeader CreateDate;
+        private Guna.UI2.WinForms.Guna2Button btn_Copy;
+    }
+}
+
---
+++ PhotoYearMover/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace PhotoYearMover
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        
+        public void init()
+        {
+            int nowYear = DateTime.Now.Year;
+
+            for (int i = nowYear - 10; i <= nowYear; i++)
+            {
+                this.comboBox1.Items.Add(i.ToString());
+            }
+
+            this.comboBox1.SelectedItem = nowYear.ToString();
+        }
+
+        private void guna2TextBox1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "請選擇存放資料夾";
+                dialog.ShowNewFolderButton = false;
+
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    guna2TextBox1.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            // 取得選擇的年份
+            var selectedYear = comboBox1.SelectedItem;
+
+            // 檢查年份是否有選
+            if (selectedYear == null)
+            {
+                MessageBox.Show("請選擇年份", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            MessageBox.Show("準備開始處理照片！", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            ScanPhotosFromCDrive();
+
+            MessageBox.Show("掃描完成！確認後請按下複製作業", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void ScanPhotosFromCDrive()
+        {
+            listLogs.Items.Clear();
+
+            //找出電腦上所有邏輯磁碟
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                if (drive.IsReady && drive.DriveType == DriveType.Fixed)
+                {
+                    ScanDirectory(drive.RootDirectory.FullName);
+                }
+            }
+        }
+
+        private void ScanDirectory(string path)
+        {
+            // 要排除的系統資料夾（可自行擴充）
+            string[] ignoreFolders = { "Windows", "Program Files", "Program Files (x86)", "ProgramData" };
+
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(path))
+                {
+                    string folderName = new DirectoryInfo(dir).Name;
+
+                    // 忽略系統資料夾
+                    if (ignoreFolders.Any(x => string.Equals(x, folderName, StringComparison.OrdinalIgnoreCase)))
+                        continue;
+
+                    // 遞迴繼續掃子資料夾
+                    ScanDirectory(dir);
+                }
+
+                foreach (string file in Directory.GetFiles(path))
+                {
+                    string ext = Path.GetExtension(file).ToLower();
+                    if (ext == ".jpg" || ext == ".jpeg")
+                    {
+                        AddToListView(file);
+                    }
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void AddToListView(string filePath)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                string createdDate = fileInfo.CreationTime.ToString("yyyy/MM/dd");
+
+                string takenDate = GetPhotoTakenDate(filePath);
+
+                string selectedYear = comboBox1.SelectedItem?.ToString();
+                if (!string.IsNullOrEmpty(selectedYear) && takenDate.StartsWith(selectedYear))
+                {
+                    ListViewItem item = new ListViewItem(filePath);
+                    item.SubItems.Add(takenDate.Substring(0,10));
+                    listLogs.Items.Add(item);
+                    listLogs.Refresh();
+                }
+            }
+            catch { }
+        }
+
+        private string GetPhotoTakenDate(string filePath)
+        {
+            try
+            {
+                using (var img = System.Drawing.Image.FromFile(filePath))
+                {
+                    const int PropertyTagExifDTOrig = 0x9003;
+                    if (img.PropertyIdList.Contains(PropertyTagExifDTOrig))
+                    {
+                        var prop = img.GetPropertyItem(PropertyTagExifDTOrig);
+                        string dateTakenStr = Encoding.ASCII.GetString(prop.Value).Trim();
+
+                        return Regex.Replace(dateTakenStr, ":", "/");
+                    }
+                }
+            }
+            catch { }
+
+            return "無拍攝日期";
+        }
+
+        private void btn_Copy_Click(object sender, EventArgs e)
+        {
+            string targetRoot = guna2TextBox1.Text.Trim();
+
+            if (string.IsNullOrEmpty(targetRoot))
+            {
+                MessageBox.Show("請選擇存放資料夾", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            foreach (ListViewItem item in listLogs.Items)
+            {
+                try
+                {
+                    string sourceFilePath = item.Text; 
+                    string takenDate = item.SubItems[1].Text; 
+
+                    string subFolderName = DateTime.Parse(takenDate).ToString("yyyy-MM");
+                    string targetFolder = Path.Combine(targetRoot, subFolderName);
+
+                    if (!Directory.Exists(targetFolder))
+                    {
+                        Directory.CreateDirectory(targetFolder);
+                    }
+
+                    string fileName = Path.GetFileName(sourceFilePath);
+                    string destPath = Path.Combine(targetFolder, fileName);
+
+                    File.Copy(sourceFilePath, destPath, true);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"複製失敗：{ex.Message}", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            MessageBox.Show("複製完成！", "成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+    }
+}
